d0bdc43e4b3aef695fea3836c6d97cd5d32ddcef
full-stack-david <davidchan12323@gmail.com>
2023-08-05 02:13:22 -0500

    Create app main screen

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * @format
+ */
+
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+
+const App = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#212765" />
+    <View
+      style={{ backgroundColor: '#212765', flex: 1 }}
+    />
+  </>
+);
+
+export default App;
